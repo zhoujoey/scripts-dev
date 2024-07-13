4f8ecb324a8302c0aebe2de2e480ec0c5d9e78a6
zhouwei <zhouwei.b@outlook.com>
2024-07-13 19:41:22 +0800

    add zsh code

--- zsh.sh
+++ zsh.sh
@@ -13,5 +13,13 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 echo 'ZSH_THEME="ys"' >>~/.zshrc
 echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting vscode z)' >>~/.zshrc
 echo 'source $ZSH/oh-my-zsh.sh' >>~/.zshrc
+echo 'setopt no_nomatch' >>~/.zshrc
+echo 'alias gcm="git commit -m"' >>~/.zshrc
+echo 'alias ga="git add ."' >>~/.zshrc
+echo 'alias gs="git status"' >>~/.zshrc
+echo 'alias dps="docker ps -a"' >>~/.zshrc
+echo 'alias dimg="docker images"' >>~/.zshrc
+echo 'alias dexe="docker exec -it --user simulator"' >>~/.zshrc
+echo 'alias vpn="export HTTP_PROXY=http://127.0.0.1:7897; export HTTPS_PROXY=http://127.0.0.1:7897; export ALL_PROXY=socks5://127.0.0.1:7897"' >>~/.zshrc
 zsh
 cd ~    
